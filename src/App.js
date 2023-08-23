c8d9dca9c85210af9ff15115bbcb789ce90b41c1
Kamil <kamilos@MacBook-Pro-Kamil.local>
2023-08-23 15:19:24 +0200

    dodanie channel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {StreamChat} from "stream-chat";
+import {Chat} from "stream-chat-react";
+import Cookies from "universal-cookie";
 
+const apiKey='8ycwk57wmbs6';
+const client =StreamChat.getInstance(apiKey)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_inside">
+      <Chat client={client} theme={"team light"}>
+        <ChannelListContainer/>
+        <ChanelContainer/>
+      </Chat>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/channelList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const channelList = () => {
+    return(
+        <div>
+            channelList
+        </div>
+    )
+}
+export default channelList;
